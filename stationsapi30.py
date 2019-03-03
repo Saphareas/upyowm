f99e37132e044fbe63f0d90eaf5a83fdaeea3c67
Fabian Große <saphareas@googlemail.com>
2019-03-03 12:06:27 +0100

    Stations API 3.0 according to owm.org

--- stationsapi30.py
+++ stationsapi30.py
@@ -1,27 +1,59 @@
 import requests as urequests
 
-def __combine_params(api_key, params=None):
-    """TODO: docstring
-    """
-    combined = "?appid={}".format(api_key)
-    if params not None:
-        for key, value in params.items():
-            url_params += "&{}={}".format(key, value)
-    return combined
-
-def post(api_key, json_payload, url="http://api.openweathermap.org/data/3.0/stations", params=None, header={"Content-Type": "application/json"}):
-    """Makes a POST request
-
-    :param url: the URL that is requested
-    TODO: docstring
-    """
-    url += __combine_params(api_key, params=params)
-    response = urequests.post(url, json=json_payload, headers=header)
+def register_station(self, api_key, eid, name, lat, long, alt):
+    """HTTP 201 if successful"""
+    url = "http://api.openweathermap.org/data/3.0/stations"
+    url += "?appid={}".format(api_key)
+    header = { "Content-Type": "application/json" }
+    payload = {
+        "external_id": eid,
+        "name": name,
+        "latitude": lat,
+        "longitude": long,
+        "altitude": alt
+    }
+    response = urequests.post(url, json=payload, headers=header)
     return response
 
-def get(api_key, url="http://api.openweathermap.org/data/3.0/stations/", params=None):
-    """TODO: docstring
+def get_stations(self, api_key, station_id=None):
+    url = "http://api.openweathermap.org/data/3.0/stations"
+    if station_id is not None:
+        url += "/{}".format(station_id)
+    url += "?appid={}".format(api_key)
+    response = urequests.get(url)
+    return response
+
+def update_station(self, api_key, station_id, **kwargs):
+    """HTTP 200 if successful"""
+    url = "http://api.openweathermap.org/data/3.0/stations"
+    url += "/{}".format(station_id)
+    url += "?appid={}".format(api_key)
+    header = { "Content-Type": "application/json" }
+    response = urequests.put(url, data=kwargs)
+    return response
+
+def delete_station(self, api_key, station_id):
+    """HTTP 204 if successful"""
+    url = "http://api.openweathermap.org/data/3.0/stations"
+    url += "/{}".format(station_id)
+    url += "?appid={}".format(api_key)
+    response = urequests.delete(url)
+    return response
+
+def upload_measurement(self, api_key, data):
+    """HTTP 204 if successful
+    :param data: should be a dict, for all parameters refer to https://openweathermap.org/stations#measurement
     """
-    url += __combine_params(api_key, params=params)
+    url = "http://api.openweathermap.org/data/3.0/measurements"
+    url += "?appid={}".format(api_key)
+    header = { "Content-Type": "application/json" }
+    response = urequests.post(url, json=data, headers=header)
+    return response
+
+def get_aggregated_measurements(self, api_key, station_id, unit_type, limit):
+    url = "http://api.openweathermap.org/data/3.0/stations"
+    url += "?appid={}".format(api_key)
+    for param in ["station_id", "unit_type", "limit"]:
+        url += "&{}={}".format(param, eval(param))
     response = urequests.get(url)
     return response
